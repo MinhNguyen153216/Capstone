607451584906b90a510dba60f2f166602cd7d4db
NguyenDangMinh <mattie1earth@gmail.com>
2022-05-21 12:02:23 +0700

    home: last commit

--- js/home.js
+++ js/home.js
@@ -20,3 +20,29 @@ function openProject(evt, projectName) {
 
 // Get the element with id="defaultOpen" and click on it
 document.getElementById("defaultOpen").click();
+
+function openProjectResp(evt, projectName) {
+  var i, tabcontent, tablinks;
+
+  tablinks = document.getElementsByClassName("tablinks-resp");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+    // tablinks[i].classList = tablinks[i].classList.remove("tab1");
+    tablinks[i].classList.remove("tab1");
+  }
+
+  tabcontent = document.getElementsByClassName("tabcontent-resp");
+  for (i = 0; i < tabcontent.length; i++) {
+    // tabcontent[i].classList.remove("displayBlockAnimate");
+    tabcontent[i].classList.add("displayNormalAnimate");
+    tabcontent[i].style.maxHeight = "0px";
+  }
+
+  // document.getElementById(projectName).classList.remove("displayNoneAnimate");
+  document.getElementById(projectName).style.maxHeight = "1000px";
+  evt.currentTarget.className += " active";
+  evt.currentTarget.classList.add("tab1");
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen-resp").click();
